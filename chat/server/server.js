764d4cbc05204a9aaf9af762924a225c8794eb20
Kim Nguyen <kim.nguyen8@griffithuni.edu.au>
2019-08-31 21:54:12 +1000

    changes to server.js as well as users service

--- chat/server/server.js
+++ chat/server/server.js
@@ -1,15 +1,27 @@
 const express = require('express')
 const app = express();
 const fs = require('fs');
-//const cors = require('http').Server(app);
+const cors = require('cors');
 //const io = require('socket.io')(http);
 //const sockets = require('.sockets/js');
-const server = require('./listen.js');
+//const server = require('./listen.js');
+var http = require('http').Server(app);
+const path = require('path');
 var bodyParser = require('body-parser');
+
+app.use(express.static(path.join(__dirname, '../dist/chat')));
+app.use(bodyParser.json());
 
 const PORT = 3000
 
-//app.use(cors());
+app.use(cors());
+
+// Testing connection
+let server = http.listen(3000, function(){
+    let host = server.address().address;
+    let port = server.address().port;
+    console.log("Server listening on: " + host + " port: " + port);
+});
 
 //sockets.connect(io, PORT)
 
@@ -17,6 +29,12 @@ const PORT = 3000
 
 //require('route.js')(app, path);
 
+app.get('/api/user', (req, res) => {
+    const username = req.query.username;
+    console.log('GET request at /api/user');
+});
+
+// Read users in database
 let users;
 fs.readFile('data.json', (error, data) =>{
     if(error) throw error;
--- chat/src/app/dashboard/dashboard.component.ts
+++ chat/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,13 +9,31 @@ import { HttpClient } from '@angular/common/http';
 export class DashboardComponent implements OnInit {
 
   username:string = localStorage.getItem('username');
-  users:[] = []
+  users;
 
-  constructor(private http: HttpClient) { }
+  constructor(private usersService: UsersService) { 
+    this.getUsers();
+  }
 
   ngOnInit() {
     console.log("Logged in as " + this.username);
-    console.log(this.username);
   }
 
+  getUsers() {
+    this.usersService.getUsers(this.username).subscribe(
+      data => {
+        this.users = data;
+      },
+      err => {
+        console.error
+      },
+      () => {
+        console.log('\tUsers retrieved')
+        console.log(this.users);
+        this.users.forEach(element => {
+          console.log(element);
+        });
+      }
+    );
+}
 }
